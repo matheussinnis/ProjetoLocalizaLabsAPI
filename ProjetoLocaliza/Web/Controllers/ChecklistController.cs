11d22b81b45c7d776a332f76201b32301e6bc708
axell-brendow <breno.axel@gmail.com>
2021-02-14 19:06:40 -0300

    Create Checklist controller

---
+++ ProjetoLocaliza/Web/Controllers/ChecklistController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Core.Entities;
+using Domain.Interfaces;
+using Microsoft.Extensions.Logging;
+
+namespace Web.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ChecklistController : CrudController<Checklist>
+    {
+        public ChecklistController(ILogger<Checklist> logger, IBaseService<Checklist> service)
+            : base(logger, service) {}
+    }
+}
