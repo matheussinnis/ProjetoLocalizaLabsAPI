cdf459c929cc134f4e7faf57bcd17581c02af0c6
axell-brendow <breno.axel@gmail.com>
2021-02-14 18:46:55 -0300

    Create Checklist entity

---
+++ ProjetoLocaliza/Core/Entities/Checklist.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Core.Entities
+{
+    public class Checklist : BaseEntity
+    {
+        [Key]
+        [ForeignKey("Schedule")]
+        public new Guid Id { get; set; }
+
+        public virtual Schedule Schedule { get; set; }
+
+        public bool Clean { get; set; }
+
+        public bool TankFull { get; set; }
+
+        public bool Dents { get; set; }
+
+        public bool Scratches { get; set; }
+
+        public int MissingLiters { get; set; }
+    }
+}
