6d2f2da68d4cd3aab3dc1d3ec27af0838cb90075
axell-brendow <breno.axel@gmail.com>
2021-02-14 18:47:03 -0300

    Create Quotation entity

---
+++ ProjetoLocaliza/Core/Entities/Quotation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Core.Entities
+{
+    public class Quotation : BaseEntity
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        public new Guid Id { get; set; }
+
+        public Guid VehicleId { get; set; }
+        public virtual Vehicle Vehicle { get; set; }
+
+        public decimal HourlyPrice { get; set; }
+
+        public decimal Total =>
+            ExpectedReturnDate.Subtract(ExpectedWithdrawalDate).Hours * HourlyPrice;
+
+        [Required]
+        public DateTime ExpectedWithdrawalDate { get; set; }
+
+        [Required]
+        public DateTime ExpectedReturnDate { get; set; }
+    }
+}
