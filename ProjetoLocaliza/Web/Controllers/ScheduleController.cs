a68dabb73a8201dd0144136ea49f4204805dc4e6
axell-brendow <breno.axel@gmail.com>
2021-02-14 19:13:16 -0300

    Create Schedule controller

---
+++ ProjetoLocaliza/Web/Controllers/ScheduleController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Core.Entities;
+using Domain.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Logging;
+
+namespace Web.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ScheduleController : CrudController<Schedule>
+    {
+        public ScheduleController(ILogger<Schedule> logger, IScheduleService service)
+            : base(logger, service) {}
+    }
+}
