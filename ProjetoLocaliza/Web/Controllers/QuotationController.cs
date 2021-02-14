cfb39d3d090039f7b400aad8f9bea15216e7ff4a
axell-brendow <breno.axel@gmail.com>
2021-02-14 19:06:47 -0300

    Create Quotation controller

---
+++ ProjetoLocaliza/Web/Controllers/QuotationController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Core.Entities;
+using Domain.Interfaces;
+using Microsoft.Extensions.Logging;
+
+namespace Web.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class QuotationController : CrudController<Quotation>
+    {
+        public QuotationController(ILogger<Quotation> logger, IQuotationService service)
+            : base(logger, service) {}
+    }
+}
