529b2153853125913bcc80a0879cacff2edd0894
axell-brendow <breno.axel@gmail.com>
2021-02-14 19:02:27 -0300

    Add library to handle cyclic loops in JSON

--- ProjetoLocaliza/Web/Startup.cs
+++ ProjetoLocaliza/Web/Startup.cs
@@ -28,7 +28,10 @@ namespace LocalizaAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers().AddNewtonsoftJson(options =>
+                options.SerializerSettings.ReferenceLoopHandling =
+                    Newtonsoft.Json.ReferenceLoopHandling.Ignore
+            );
             services.AddDbContext<DataContext>();
             services.AddScoped(typeof(IBaseRepository<>), typeof(BaseRepository<>));
             services.AddScoped(typeof(IBaseService<>), typeof(BaseService<>));
