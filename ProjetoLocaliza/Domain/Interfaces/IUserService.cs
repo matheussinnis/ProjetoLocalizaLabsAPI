5c6dfd6489307e0f3056412a7a0307824c85ca8d
axell-brendow <breno.axel@gmail.com>
2021-02-14 18:59:28 -0300

    Create UserService

---
+++ ProjetoLocaliza/Domain/Interfaces/IUserService.cs
@@ -0,0 +1,8 @@
+using Core.Entities;
+
+namespace Domain.Interfaces
+{
+    public interface IUserService : IBaseService<User>
+    {
+    }
+}
---
+++ ProjetoLocaliza/Domain/Services/UserService.cs
@@ -0,0 +1,31 @@
+using System.Threading.Tasks;
+using Core.Entities;
+using Domain.Exceptions;
+using Domain.Interfaces;
+using Infrastructure.Auth;
+using Infrastructure.Database.Interfaces;
+
+namespace Domain.Services
+{
+    public class UserService : BaseService<User>, IUserService
+    {
+        public UserService(IBaseRepository<User> repository) : base(repository) {}
+
+        public new async Task<User> AddAsync(User quotation)
+        {
+            quotation.Password = PasswordEncryptor.Encrypt(quotation.Password);
+            await _repository.AddAsync(quotation);
+            await _repository.SaveChangesAsync();
+            return quotation;
+        }
+
+        public override async Task<User> FindAsync(string id)
+        {
+            var entity = await _repository.FindAsync(id, include => include.Address);
+            if (entity == null)
+                throw new NotFoundException($"{typeof(User).Name} não encontrado(a)");
+
+            return entity;
+        }
+    }
+}
