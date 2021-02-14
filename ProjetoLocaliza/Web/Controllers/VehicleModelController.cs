aa8366aae3ace817286753dfdeed814044386fc2
axell-brendow <breno.axel@gmail.com>
2021-02-14 19:08:24 -0300

    Update VehicleModel controller

--- ProjetoLocaliza/Web/Controllers/VehicleModelController.cs
+++ ProjetoLocaliza/Web/Controllers/VehicleModelController.cs
@@ -1,8 +1,5 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.Threading.Tasks;
 using Core.Entities;
 using Domain.Interfaces;
@@ -10,36 +7,33 @@ using Microsoft.Extensions.Logging;
 
 namespace Web.Controllers
 {
-
     [ApiController]
     [Route("api/[controller]")]
-    public class VehicleModelController : Controller
+    public class VehicleModelController : CrudController<VehicleModel>
     {
-
-        private readonly ILogger<VehicleModel> _logger;
-        private readonly IBaseService<VehicleModel> _vehicleModelService;
-
-        public VehicleModelController(ILogger<VehicleModel> logger, IBaseService<VehicleModel> vehicleModelService)
-        {
-            _logger = logger;
-            _vehicleModelService = vehicleModelService;
-        }
+        public VehicleModelController(
+            ILogger<VehicleModel> logger, IBaseService<VehicleModel> service
+        ) : base(logger, service) {}
 
         [HttpPost]
         [Authorize(Roles = "Operator")]
-        public async Task<IActionResult> Create([FromBody] VehicleModel vehicleModel)
+        public override Task<IActionResult> Create([FromBody] VehicleModel vehicleModel)
         {
-            try
-            {
-                await _vehicleModelService.AddAsync(vehicleModel);
-                return StatusCode(201);
-            }
+            return base.Create(vehicleModel);
+        }
 
-            catch (Exception exception)
-            {
-                _logger.Log(LogLevel.Error, exception, exception.Message);
-                return StatusCode(500, new { Message = exception.Message });
-            }
+        [HttpPut("{id}")]
+        [Authorize(Roles = "Operator")]
+        public override Task<IActionResult> Update(string id, [FromBody] VehicleModel vehicleModel)
+        {
+            return base.Update(id, vehicleModel);
+        }
+
+        [HttpDelete("{id}")]
+        [Authorize(Roles = "Operator")]
+        public override Task<IActionResult> Delete(string id)
+        {
+            return base.Delete(id);
         }
     }
 }
