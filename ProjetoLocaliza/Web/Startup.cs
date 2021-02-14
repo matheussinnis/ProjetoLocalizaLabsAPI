23b877ab09cdd0f6de5c9f0a7c32ba818a68fa4d
axell-brendow <breno.axel@gmail.com>
2021-02-14 19:03:22 -0300

    Add JWT Authorization for Swagger

--- ProjetoLocaliza/Web/Startup.cs
+++ ProjetoLocaliza/Web/Startup.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Text;
 using Domain.Interfaces;
 using Domain.Services;
@@ -66,6 +67,33 @@ namespace LocalizaAPI
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "LocalizaAPI", Version = "v1" });
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = @"JWT Authorization header using the Bearer scheme. Example: 'Bearer 12345abcdef'",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            },
+                            Scheme = "oauth2",
+                            Name = "Bearer",
+                            In = ParameterLocation.Header,
+
+                        },
+                        new List<string>()
+                    }
+                });
             });
         }
 
@@ -82,7 +110,7 @@ namespace LocalizaAPI
             app.UseHttpsRedirection();
 
             app.UseRouting();
-            
+
             app.UseCors(x => x
                 .AllowAnyOrigin()
                 .AllowAnyMethod()
