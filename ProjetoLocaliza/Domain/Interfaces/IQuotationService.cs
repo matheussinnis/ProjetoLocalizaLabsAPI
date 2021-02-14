45a858303bf747fe0da811bd521172a7b647bfed
axell-brendow <breno.axel@gmail.com>
2021-02-14 18:57:25 -0300

    Create QuotationService

---
+++ ProjetoLocaliza/Domain/Interfaces/IQuotationService.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Core.Entities;
+
+namespace Domain.Interfaces
+{
+    public interface IQuotationService : IBaseService<Quotation>
+    {
+    }
+}
---
+++ ProjetoLocaliza/Domain/Services/QuotationService.cs
@@ -0,0 +1,28 @@
+using System.Threading.Tasks;
+using Core.Entities;
+using Domain.Interfaces;
+using Infrastructure.Database.Interfaces;
+
+namespace Domain.Services
+{
+    public class QuotationService : BaseService<Quotation>, IQuotationService
+    {
+        public QuotationService(IBaseRepository<Quotation> repository) : base(repository) {}
+
+        public new async Task<Quotation> AddAsync(Quotation quotation)
+        {
+            quotation.HourlyPrice = quotation.Vehicle.HourlyPrice;
+            await _repository.AddAsync(quotation);
+            await _repository.SaveChangesAsync();
+            return quotation;
+        }
+
+        public new async Task<Quotation> UpdateAsync(Quotation quotation)
+        {
+            quotation.HourlyPrice = quotation.Vehicle.HourlyPrice;
+            _repository.Update(quotation);
+            await _repository.SaveChangesAsync();
+            return quotation;
+        }
+    }
+}
