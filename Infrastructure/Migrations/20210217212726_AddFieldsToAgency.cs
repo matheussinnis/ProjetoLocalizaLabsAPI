7b3d6cce24e9bb6298cc25d4e4b7cf336bbf33cb
axell-brendow <breno.axel@gmail.com>
2021-02-17 20:53:34 -0300

    Add migrations

---
+++ Infrastructure/Migrations/20210217212726_AddFieldsToAgency.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastructure.Migrations
+{
+    public partial class AddFieldsToAgency : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "Latitude",
+                table: "Agency",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Longitude",
+                table: "Agency",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Phone",
+                table: "Agency",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Whatsapp",
+                table: "Agency",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Latitude",
+                table: "Agency");
+
+            migrationBuilder.DropColumn(
+                name: "Longitude",
+                table: "Agency");
+
+            migrationBuilder.DropColumn(
+                name: "Phone",
+                table: "Agency");
+
+            migrationBuilder.DropColumn(
+                name: "Whatsapp",
+                table: "Agency");
+        }
+    }
+}
