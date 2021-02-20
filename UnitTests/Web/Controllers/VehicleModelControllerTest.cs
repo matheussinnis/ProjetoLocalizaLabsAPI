bdb2f4aad53723fa33b7e51507a2e3f27dae479a
axell-brendow <breno.axel@gmail.com>
2021-02-20 03:58:09 -0300

    Create VehicleModelController tests

---
+++ UnitTests/Web/Controllers/VehicleModelControllerTest.cs
@@ -0,0 +1,206 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Core.Entities;
+using Domain.Exceptions;
+using Domain.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Primitives;
+using Moq;
+using Web.Controllers;
+using Xunit;
+
+namespace UnitTests.Web.Controllers
+{
+    public class VehicleModelControllerTest
+    {
+        private VehicleModelController _controller { get; set; }
+        private Mock<IBaseService<VehicleModel>> _service { get; set; }
+
+        public VehicleModelControllerTest()
+        {
+            var logger = new Mock<ILogger<VehicleModel>>();
+            _service = new Mock<IBaseService<VehicleModel>>();
+            _controller = new VehicleModelController(logger.Object, _service.Object);
+        }
+
+        [Fact]
+        public async void TestGetAll()
+        {
+            _service.Setup(s => s.GetAllAsync()).Returns(
+                Task.FromResult(Enumerable.Empty<VehicleModel>())
+            );
+
+            var httpContext = new DefaultHttpContext();
+            _controller.ControllerContext = new ControllerContext()
+            {
+                HttpContext = httpContext
+            };
+            var response = await _controller.GetAll();
+
+            _service.Verify(s => s.GetAllAsync(), Times.Once);
+            Assert.Equal(200, response.StatusCode);
+            Assert.Equal(Enumerable.Empty<VehicleModel>(), response.Value);
+        }
+
+        [Fact]
+        public async void TestGetAllWithName()
+        {
+            var httpContext = new DefaultHttpContext();
+            httpContext.Request.Query = new QueryCollection(
+                new Dictionary<string, StringValues>()
+                {
+                    { "name", new StringValues("nameValue") }
+                }
+            );
+
+            _service.Setup(s => s.FilterAsync(
+                vehicleModel => vehicleModel.Name.Contains(
+                    httpContext.Request.Query["name"]
+                )
+            )).Returns(Task.FromResult(Enumerable.Empty<VehicleModel>()));
+
+            _controller.ControllerContext = new ControllerContext()
+            {
+                HttpContext = httpContext
+            };
+            var response = await _controller.GetAll();
+
+            Assert.Equal(200, response.StatusCode);
+            Assert.Equal(Enumerable.Empty<VehicleModel>(), response.Value);
+        }
+
+        [Fact]
+        public async void TestGetAllStatus500()
+        {
+            _service.Setup(s => s.GetAllAsync()).Throws(new Exception());
+
+            var response = await _controller.GetAll();
+
+            Assert.Equal(500, response.StatusCode);
+        }
+
+        [Fact]
+        public async void TestFind()
+        {
+            var guid = Guid.NewGuid();
+            var id = guid.ToString();
+            var modelMock = new VehicleModel() { Id = guid };
+            _service.Setup(s => s.FindAsync(id)).Returns(Task.FromResult(modelMock));
+
+            var response = await _controller.Find(id);
+
+            Assert.Equal(200, response.StatusCode);
+            Assert.Equal(modelMock, response.Value);
+        }
+
+        [Fact]
+        public async void TestFind404()
+        {
+            var id = "";
+            _service.Setup(s => s.FindAsync(id)).Throws(new NotFoundException());
+
+            var response = await _controller.Find(id);
+
+            Assert.Equal(404, response.StatusCode);
+        }
+
+        [Fact]
+        public async void TestFind500()
+        {
+            var id = "";
+            _service.Setup(s => s.FindAsync(id)).Throws(new Exception());
+
+            var response = await _controller.Find(id);
+
+            Assert.Equal(500, response.StatusCode);
+        }
+
+        [Fact]
+        public async void TestCreate()
+        {
+            var guid = Guid.NewGuid();
+            var modelMock = new VehicleModel() { Id = guid };
+            _service.Setup(s => s.AddAsync(modelMock)).Returns(Task.FromResult(modelMock));
+
+            var response = await _controller.Create(modelMock);
+
+            Assert.Equal(201, response.StatusCode);
+            Assert.Equal(modelMock, response.Value);
+        }
+
+        [Fact]
+        public async void TestCreate500()
+        {
+            var guid = Guid.NewGuid();
+            var modelMock = new VehicleModel() { Id = guid };
+            _service.Setup(s => s.AddAsync(modelMock)).Throws(new Exception());
+
+            var response = await _controller.Create(modelMock);
+
+            Assert.Equal(500, response.StatusCode);
+        }
+
+        [Fact]
+        public async void TestUpdate()
+        {
+            var guid = Guid.NewGuid();
+            var modelMock = new VehicleModel() { Id = guid };
+            _service.Setup(s => s.UpdateAsync(modelMock)).Returns(Task.FromResult(modelMock));
+
+            var response = await _controller.Update(modelMock.Id.ToString(), modelMock);
+
+            Assert.Equal(200, response.StatusCode);
+            Assert.Equal(modelMock, response.Value);
+        }
+
+        [Fact]
+        public async void TestUpdate500()
+        {
+            var guid = Guid.NewGuid();
+            var modelMock = new VehicleModel() { Id = guid };
+            _service.Setup(s => s.UpdateAsync(modelMock)).Throws(new Exception());
+
+            var response = await _controller.Update(modelMock.Id.ToString(), modelMock);
+
+            Assert.Equal(500, response.StatusCode);
+        }
+
+        [Fact]
+        public async void TestDelete()
+        {
+            var guid = Guid.NewGuid();
+            var id = guid.ToString();
+            _service.Setup(s => s.DeleteAsync(id)).Returns(Task.CompletedTask);
+
+            var response = await _controller.Delete(id);
+
+            Assert.Equal(204, response.StatusCode);
+        }
+
+        [Fact]
+        public async void TestDelete404()
+        {
+            var id = "";
+            _service.Setup(s => s.DeleteAsync(id)).Throws(new NotFoundException());
+
+            var response = await _controller.Delete(id);
+
+            Assert.Equal(404, response.StatusCode);
+        }
+
+        [Fact]
+        public async void TestDelete500()
+        {
+            var id = "";
+            _service.Setup(s => s.DeleteAsync(id)).Throws(new Exception());
+
+            var response = await _controller.Delete(id);
+
+            Assert.Equal(500, response.StatusCode);
+        }
+    }
+}
